a3fc3ff633f23ccead14aa05a92f20bc96a5c8a1
bunny-ml <abhishek260204@gmail.com>
2025-12-10 21:12:46 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,38 +2,23 @@ import streamlit as st
 import torch
 from PIL import Image
 from model.model import CustomTrOCR
-from transformers import AutoConfig, AutoProcessor
 
 st.title("Handwriting Recognition (TrOCR + LoRA)")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 @st.cache_resource
 def load_model():
     repo_id = "scientist-bunny/coustom_ocr_handwritten_model"
-
-    # Load config + processor
-    config = AutoConfig.from_pretrained(repo_id)
-    processor = AutoProcessor.from_pretrained(repo_id)
-
-    # Initialize the model
-    model = CustomTrOCR(config)
-
-    # Load weights from HF (.pt file)
-    weights = torch.hub.load_state_dict_from_url(
-        f"https://huggingface.co/{repo_id}/resolve/main/weights.pt",
-        map_location=device
+    model, processor = CustomTrOCR.from_pretrained(
+        repo_id,
+        filename="weights.pt",
+        device=device
     )
-    model.load_state_dict(weights)
-
-    model.to(device)
-    model.eval()
-
-    return model, processor
+    return model, processor, device
 
 
-model, processor = load_model()
+model, processor, device = load_model()
 
 
 # UI
@@ -43,10 +28,8 @@ if img_file:
     image = Image.open(img_file).convert("RGB")
     st.image(image, width=300)
 
-    # Preprocess
     pixel_values = processor(image, return_tensors="pt").pixel_values.to(device)
 
-    # Generate output text
     ids = model.generate(pixel_values, max_length=64)
     text = processor.batch_decode(ids, skip_special_tokens=True)[0]
 
